b94bc336e50f0d2e1d0f01968910c2960c64315c
ynozar <ynozar@calpoly.edu>
2021-06-17 11:17:24 -0700

    Update CMACEventBrite.js

--- CMACEventBrite.js
+++ CMACEventBrite.js
@@ -1,12 +1,15 @@
 
-    var exampleCallback = function() {
+   var exampleCallback = function() {
         console.log('Order complete!');
     };
 
     window.EBWidgets.createWidget({
+        // Required
         widgetType: 'checkout',
-        eventId: '119300716655',
-        modal: true,
-        modalTriggerElementId: 'eventbrite-widget-modal-trigger-119300716655',
-        onOrderComplete: exampleCallback
+        eventId: '156963960431',
+        iframeContainerId: 'eventbrite-widget-container-156963960431',
+
+        // Optional
+        iframeContainerHeight: 425,  // Widget height in pixels. Defaults to a minimum of 425px if not provided
+        onOrderComplete: exampleCallback  // Method called when an order has successfully completed
     });
